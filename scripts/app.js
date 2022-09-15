68a4b1170b2a030900d674fc78aa6c82ca6ecfb3
Emanuel <emanuel.sotomayor.sec@gmail.com>
2022-09-15 18:34:43 -0300

    Tab slider terminado y SASS agregado

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,8 @@
 const menuContainer = document.querySelector("#menuContainer");
 const btnMenu = document.querySelector("#open_menu");
 const closeBtnMenu = document.querySelector("#close_menu");
-const allGalleries = document.querySelectorAll(".gallery");
-const allNavsLink = document.querySelectorAll(".nav__slider--link");
-const firstNavLink = document.querySelector("#first__nav--link");
+const allGalleries = Array.from(document.querySelectorAll(".gallery"));
+const allTabs = Array.from(document.querySelectorAll(".tab"));
 
 btnMenu.addEventListener("click", ()=>{
     menuContainer.style.left = "0";
@@ -20,6 +19,34 @@ closeBtnMenu.addEventListener("click", ()=>{
     closeBtnMenu.classList.remove("showCloseMenu");
 });
 
-allNavsLink.forEach((link)=>{
-    
+/*El primer tab arranca coloreado*/
+allTabs[0].style.color = "#EF3194";
+
+/*Recorremos todos los tabs y le damos un evento click a cada item*/
+allTabs.forEach((tab, index)=>{
+
+    tab.addEventListener("click", ()=>{
+
+        /*En cada acción del click, se recorre todos los items de gallery
+        y sí tienen la clase 'gallery-active' se las quita, para ocultarlos*/
+        allGalleries.forEach((galleryTemp)=>{
+            galleryTemp.classList.remove("gallery-active");
+        });
+
+        /*En cada acción del click, se recorre todos los items de tabs
+        y sí tienen la clase 'active' se las quita*/
+        allTabs.forEach((tabTemp)=>{
+            tabTemp.classList.remove("active");
+            /*En cada iteración hace que el tab se vuelva blanco,
+            sí hacemos click*/
+            tabTemp.style.color = "#FFFFFF";
+        });
+
+        allGalleries[index].classList.add("gallery-active");
+        allTabs[index].classList.add("active");
+        /*Le pasamos el parámetro index como indice,
+        para colorear el tab actual*/
+        allTabs[index].style.color = "#EF3194";
+    });
+
 });
